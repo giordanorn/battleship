fdb4a5a44cb5387d435e80c20d74758dcd0fdf62
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-10-28 21:05:29 -0300

    add ship insertion method, now using pointers

--- utils.c
+++ utils.c
@@ -2,6 +2,8 @@
 #include "game.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
 void
 start_new_game (void)
@@ -18,15 +20,16 @@ start_new_game (void)
         .height = 10
     };
     
-    initialize_board (player);
-    initialize_board (enemy);
+    initialize_board (&player);
+    initialize_board (&enemy);
+
+    srand (time (NULL));
+    setup_board (&player);
+    setup_board (&enemy);
 
     // printing is for debugging purposes :P
     print_board (player); 
     print_board (enemy);
-
-    //setup_board (player);
-    //setup_board (enemy);
     
     start_game ();
 }
@@ -59,16 +62,121 @@ get_menu_option (void)
 void
 print_board (Board board)
 {
-    printf ("Printing %s's board:\n", board.owner);
+    printf ("%s's board:\n", board.owner);
     for (int i = 0; i < board.height; i++)
+    {
         for (int j = 0; j < board.width; j++)
-            printf ("board.grid[%d][%d] = %d\n", i, j, board.grid[i][j]);
+            printf (" %d", board.grid[i][j]);
+        printf ("\n");
+    }
 }
 
 void
-initialize_board (Board board)
+initialize_board (Board *board)
 {
-    for (int i = 0; i < board.height; i++)
-        for (int j = 0; j < board.width; j++)
-            board.grid[i][j] = WATER;
+    for (int i = 0; i < board->height; i++)
+        for (int j = 0; j < board->width; j++)
+            board->grid[i][j] = WATER;
 }
+
+void
+setup_board (Board *board)
+{
+    insert_carrier (board);
+    insert_battleship (board);
+    insert_destroyer (board);
+    insert_submarine (board);
+    insert_patrol_boat (board);
+}
+
+void
+insert_carrier (Board *board)
+{
+    insert_ship (board, 5, CARRIER);
+}
+
+void
+insert_battleship (Board *board)
+{
+    insert_ship (board, 4, BATTLESHIP);
+}
+
+void
+insert_destroyer (Board *board)
+{
+    insert_ship (board, 3, DESTROYER);
+}
+
+void
+insert_submarine (Board *board)
+{
+    insert_ship (board, 3, SUBMARINE);
+}
+
+void
+insert_patrol_boat (Board *board)
+{
+    insert_ship (board, 2, PATROL_BOAT);
+}
+
+void
+insert_ship (Board *board, int ship_size, Point ship_type)
+{
+    Orientation orientation = rand () % 2;
+    if (orientation == HORIZONTAL)
+    {
+        bool
+        seed_validity_check (int x, int y)
+        {
+            for (int i = 0; i < ship_size; ++i)
+            {
+                // conditions for avoiding accessing non-existent coordinates
+                if ( y == 0 )
+                    if (
+                        board->grid[  y  ][x + i] != WATER ||
+                        board->grid[y + 1][x + i] != WATER
+                       )
+                        return false;
+                else if ( y == board->height - 1 )
+                    if (
+                        board->grid[y - 1][x + i] != WATER ||
+                        board->grid[  y  ][x + i] != WATER
+                       )
+                        return false;
+                else
+                    if (
+                        board->grid[y - 1][x + i] != WATER ||
+                        board->grid[  y  ][x + i] != WATER ||
+                        board->grid[y + 1][x + i] != WATER
+                       )
+                        return false;
+            }
+            return true;
+        }
+                
+        // try to generate a seed (starting point) for the ship
+        int xseed, yseed;
+        do {
+            xseed = rand () % (board->width - ship_size);
+            yseed = rand () % board->height;
+        } while ( ! seed_validity_check (xseed, yseed) );
+
+        printf ("Inserting a ship of size %d at coordinate (%d,%d) in horizontal orientation.\n", ship_size, xseed, yseed);
+
+        // inserts ship for real
+        for (int i = 0; i < ship_size; ++i)
+            board->grid[yseed][xseed + i] = ship_type;
+    }
+    else
+    {
+        int xseed, yseed;
+        xseed = rand () % board->width;
+        yseed = rand () % (board->height - ship_size);
+
+        printf ("Inserting a ship of size %d at coordinate (%d,%d) in vertical orientation.\n", ship_size, xseed, yseed);
+
+        for (int i = 0; i < ship_size; ++i)
+            board->grid[yseed + i][xseed] = ship_type;
+    }
+}
+
--- utils.h
+++ utils.h
@@ -13,11 +13,11 @@ typedef enum {
 
 typedef enum {
     WATER,
-    CARRIER,	
-    BATTLESHIP,
-    DESTROYER,
-    SUBMARINE,
-    PATROL_BOAT
+    CARRIER,	// it occupies 5 points
+    BATTLESHIP, // it occupies 4 points
+    DESTROYER,  // it occupies 3 points
+    SUBMARINE,  // it occupies 3 points
+    PATROL_BOAT // it occupies 2 points
 } Point;
 
 typedef Point Grid[10][10];
@@ -29,8 +29,13 @@ typedef struct {
     Grid grid;
 } Board;
 
+typedef enum {
+    HORIZONTAL,
+    VERTICAL
+} Orientation;
+
 /*
- * Procedures
+ * Procedures and functions
  */
 
 void start_new_game (void);
@@ -38,6 +43,14 @@ void load_game (void);
 void display_menu (void);
 MenuOption get_menu_option (void);
 void print_board (Board);
-void initialize_board (Board);
+void initialize_board (Board*);
+void setup_board (Board*);
+void insert_ship (Board*, int, Point);
+void insert_carrier (Board*);
+void insert_battleship (Board*);
+void insert_destroyer (Board*);
+void insert_submarine (Board*);
+void insert_patrol_boat (Board*);
+
 
 #endif
